7ea9ec03085a282320097878682bf91804b98b2f
Tomáš Križák <tomaskrizak@gmail.com>
2024-08-14 19:54:51 +0200

    add interactive form modal and basic css

--- library.js
+++ library.js
@@ -1,8 +1,11 @@
-const booksContainer = document.querySelector('.books-container');
+const booksGrid = document.querySelector('.books__grid');
+const addBookBtn = document.querySelector('.add-book-button');
+const formModal = document.getElementById('form-modal');
+const closeFormBtn = document.querySelector('.modal__button--close');
 
 const myLibrary = [
   {
-    author: 'Fil',
+    author: 'Filip',
     title: 'harry potter',
     pages: '22',
   },
@@ -26,7 +29,7 @@ function Book(author, title, pages) {
   this.pages = pages;
 }
 
-// Handles adding individual book object to the library
+// Handles adding individual book to the book library
 
 function addBookToLibrary(author, title, pages) {
   myLibrary.push(new Book(author, title, pages));
@@ -36,11 +39,51 @@ function addBookToLibrary(author, title, pages) {
 
 console.log(myLibrary);
 
-// Displays books inside myLibrary on the page
+// Handles displaying the book form
+
+function showFormModal() {
+  document.getElementById('form-modal').style.display = 'flex';
+}
+
+function hideFormModal() {
+  document.getElementById('form-modal').style.display = 'none';
+}
+
+addBookBtn.addEventListener('click', () => {
+  showFormModal();
+});
+
+closeFormBtn.addEventListener('click', () => {
+  hideFormModal();
+});
+
+// Hides form modal if the user clicks outside of modal interface
+
+document.getElementById('form-modal').addEventListener('click', (event) => {
+  if (event.target === document.getElementById('form-modal')) {
+    hideFormModal();
+  }
+});
+
+// Handles book submission
+
+document.getElementById('book-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const author = document.getElementById('author').value;
+  const title = document.getElementById('title').value;
+  const pages = document.getElementById('pages').value;
+
+  addBookToLibrary(author, title, pages);
+
+  hideFormModal();
+});
+
+// Displays submitted book inside myLibrary on the page
 
 function displayBooks() {
-  // Clear current book list to prevent duplication
-  booksContainer.innerHTML = '';
+  // Clear current book list to prevent book duplication
+  booksGrid.innerHTML = '';
 
   myLibrary.forEach((book) => {
     // Create elements for each book
@@ -50,13 +93,13 @@ function displayBooks() {
     const bookTitle = document.createElement('p');
     const bookPages = document.createElement('p');
 
-    // Give each book text
+    // Give each book correct text
     bookAuthor.textContent = book.author;
     bookTitle.textContent = book.title;
-    bookPages.textContent = book.pages;
+    bookPages.textContent = `${book.pages} pages`;
 
     // Add correct elements to correct DOM node
-    booksContainer.appendChild(bookDiv);
+    booksGrid.appendChild(bookDiv);
     bookDiv.appendChild(bookAuthor);
     bookDiv.appendChild(bookTitle);
     bookDiv.appendChild(bookPages);
@@ -64,15 +107,3 @@ function displayBooks() {
 }
 
 displayBooks();
-
-// Book submission
-
-document.getElementById('bookForm').addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const author = document.getElementById('author').value;
-  const title = document.getElementById('title').value;
-  const pages = document.getElementById('pages').value;
-
-  addBookToLibrary(author, title, pages);
-});
