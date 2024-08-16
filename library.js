8ad9206f5d07f765c448777a7ddfa2abcb1a6158
Tomáš Križák <tomaskrizak@gmail.com>
2024-08-16 18:12:31 +0200

    modify css element styling and transitions of modal, highlight incorrect input in modal

--- library.js
+++ library.js
@@ -1,5 +1,6 @@
 const addBookBtn = document.querySelector('.add-book-button');
 const formModal = document.getElementById('formModal');
+const bookForm = document.getElementById('bookForm');
 const closeFormBtn = document.querySelector('.modal__button--close');
 
 const myLibrary = [
@@ -7,66 +8,50 @@ const myLibrary = [
     author: 'J. K. Rowling',
     title: 'Harry Potter and the Prisoner of Azkaban',
     pages: '317',
-    isRead: false,
+    isReadStatus: true,
   },
   {
     author: 'J.R.R. Tolkien',
     title: 'The Fellowship of the Ring',
     pages: '417',
-    isRead: false,
+    isReadStatus: false,
   },
   {
     author: 'Frank Herbert',
     title: 'Dune',
     pages: '412',
-    isRead: false,
+    isReadStatus: true,
   },
 ];
 
-// Book constructor
+// Book object constructor
 
-function Book(author, title, pages, isRead) {
+function Book(author, title, pages, isReadStatus) {
   this.author = author;
   this.title = title;
   this.pages = pages;
-  this.isRead = isRead;
+  this.isReadStatus = isReadStatus;
 }
-
-// Handles adding individual book to the book library
-
-function addBookToLibrary(author, title, pages, readCheckbox) {
-  const isRead = readCheckbox.checked;
-
-  myLibrary.push(new Book(author, title, pages, isRead));
-  console.log(myLibrary);
-  displayBooks();
-}
-
-console.log(myLibrary);
-
-// Handles book submission
-
-document.getElementById('bookForm').addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const author = document.getElementById('author').value;
-  const title = document.getElementById('title').value;
-  const pages = document.getElementById('pages').value;
-  const readCheckbox = document.getElementById('read');
-
-  addBookToLibrary(author, title, pages, readCheckbox);
-
-  hideFormModal();
-});
 
 // Handles displaying the book form
 
 function showFormModal() {
-  document.getElementById('formModal').style.display = 'flex';
+  formModal.classList.add('active');
 }
 
 function hideFormModal() {
-  document.getElementById('formModal').style.display = 'none';
+  formModal.classList.remove('active');
+  clearFormErrors();
+  bookForm.reset();
+}
+
+// Removes errors from the form
+
+function clearFormErrors() {
+  const authorInput = document.getElementById('author');
+  const titleInput = document.getElementById('title');
+  authorInput.classList.remove('input-error');
+  titleInput.classList.remove('input-error');
 }
 
 addBookBtn.addEventListener('click', () => {
@@ -85,6 +70,49 @@ document.getElementById('formModal').addEventListener('click', (event) => {
   }
 });
 
+// Handles book submission
+
+bookForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const authorInput = document.getElementById('author');
+  const titleInput = document.getElementById('title');
+  const pagesInput = document.getElementById('pages');
+
+  const author = authorInput.value.trim();
+  const title = titleInput.value.trim();
+  const pages = pagesInput.value.trim();
+  const isReadStatus = document.getElementById('read').checked;
+
+  // Handles empty required input fielnds
+
+  let isValid = true;
+
+  if (!author) {
+    authorInput.classList.add('input-error');
+    isValid = false;
+  }
+
+  if (!title) {
+    titleInput.classList.add('input-error');
+    isValid = false;
+  }
+
+  if (isValid) {
+    addBookToLibrary(author, title, pages, isReadStatus);
+    hideFormModal();
+    bookForm.reset();
+  }
+});
+
+// Handles adding individual book to the book library
+
+function addBookToLibrary(author, title, pages, isReadStatus) {
+  myLibrary.push(new Book(author, title, pages, isReadStatus));
+
+  displayBooks();
+}
+
 // Displays submitted book inside myLibrary as a table on the page
 
 function displayBooks() {
@@ -93,6 +121,8 @@ function displayBooks() {
     .querySelector('tbody');
 
   booksTableBody.innerHTML = '';
+
+  // Loops through books object and generates table content
 
   myLibrary.forEach((book, index) => {
     const row = document.createElement('tr');
@@ -105,26 +135,29 @@ function displayBooks() {
 
     titleCell.textContent = book.title;
     authorCell.textContent = book.author;
-    pagesCell.textContent = `${book.pages} pages`;
+    pagesCell.textContent = !book.pages ? '' : `${book.pages} pages`;
+
+    // Setup for status button
 
     const statusButton = document.createElement('button');
-    statusButton.textContent = book.isRead ? 'Read' : 'Not Read';
-    statusButton.className = book.isRead
+    statusButton.textContent = book.isReadStatus ? 'Read' : 'Not Read';
+    statusButton.className = book.isReadStatus
       ? 'status-button-read'
       : 'status-button-not-read';
 
-    // User can change read status
+    // Toggle that lets user change if the book was read or not
 
     statusButton.addEventListener('click', () => {
-      book.isRead = !book.isRead;
-      statusButton.textContent = book.isRead ? 'Read' : 'Not Read';
-      statusButton.className = book.isRead
+      book.isReadStatus = !book.isReadStatus;
+      statusButton.textContent = book.isReadStatus ? 'Read' : 'Not Read';
+      statusButton.className = book.isReadStatus
         ? 'status-button-read'
         : 'status-button-not-read';
-      console.log(myLibrary);
     });
 
     statusCell.appendChild(statusButton);
+
+    // Removes book from library
 
     const removeButton = document.createElement('button');
     removeButton.textContent = 'Remove';
@@ -132,7 +165,6 @@ function displayBooks() {
     removeButton.addEventListener('click', () => {
       myLibrary.splice(index, 1);
       displayBooks();
-      console.log(myLibrary);
     });
 
     removalCell.appendChild(removeButton);
